b786ab09fe9ece5d0c07e5cc9a34d32fd1a019b8
mc selvan <selvan@itresonance.com>
2023-04-26 11:16:20 +0000

    updates

--- webapp/Component.js
+++ webapp/Component.js
@@ -29,6 +29,10 @@ sap.ui.define([
 
                 // set the device model
                 this.setModel(models.createDeviceModel(), "device");
+
+                // set the globalProperties model
+			    const oGlobalModel = new sap.ui.model.json.JSONModel({selIdocs: ""});
+                this.setModel(oGlobalModel, "globalProperties");
             }
         });
     }
